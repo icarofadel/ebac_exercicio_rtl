2b154ccdad983a96dca5051d711ed7a6ab36c8e1
RedSkull_7_7 <icaro.nata147@gmail.com>
2024-04-02 20:57:28 -0300

    terminando testes de comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,30 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar dois comentario', () => {
+        render(<PostComment />)
+
+        // 1º comentario
+        fireEvent.change(screen.getByTestId('comentario-textarea'), {
+            target: {
+                value: 'Comentário adicionado via testes',
+            }
+        })
+        fireEvent.click(screen.getByTestId('comentario-btn'))
+
+        //2º comentario
+        fireEvent.change(screen.getByTestId('comentario-textarea'), {
+            target: {
+                value: 'Comentário adicionado via testes',
+            }
+        })
+        fireEvent.click(screen.getByTestId('comentario-btn'))
+
+        expect(screen.getAllByTestId('elemento-comentario')).toHaveLength(2);
 });
+})
